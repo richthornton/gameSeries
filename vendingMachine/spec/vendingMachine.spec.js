c1e8c6079091fcacb6f662cdfd26545012253b34
Flora Davies <daviesfgo@gmail.com>
2021-08-02 17:41:34 +0100

    refactored tests

--- vendingMachine/spec/vendingMachine.spec.js
+++ vendingMachine/spec/vendingMachine.spec.js
@@ -3,12 +3,8 @@ const Machine = require("../src/vendingMachine");
 describe("The Vending Machine", () => {
   const vendingMachine = new Machine();
 
-  it("returns 'Vending Smarties' when correct item code and money is given", () => {
+  it("returns the correct item when correct item code and money is given", () => {
     expect(vendingMachine.vend("A01", 0.6)).toEqual("Vending Smarties");
-  });
-
-  it("returns 'Vending Freddo' when correct item code and money is given", () => {
-    expect(vendingMachine.vend("A04", 0.25)).toEqual("Vending Freddo");
   });
 
   xit("returns 'Not enough money!' when money input is less than price of item", () => {
